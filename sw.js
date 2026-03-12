ab3d3270adbf481d6befc64701176a6505692e17
shamrai78-ops <shamrai78@gmail.com>
2026-03-12 10:17:17 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'gymtraker-v3';
+const ASSETS = ['/-GymFitTracker-/', '/-GymFitTracker-/index.html'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
